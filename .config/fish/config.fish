e72018a43c562ea07bbd16375551152db2853112
Ryan C Phelps <ryancerium@gmail.com>
2020-02-01 09:55:40 -0800

    .config/fish/config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+set PATH \
+    "/home/rphelps/Android/Sdk/platform-tools" \
+    $PATH \
+    "/home/rphelps/Dropbox/bin" \
+    "/home/rphelps/.local/bin" \
+    "/home/rphelps/flutter/bin" \
+    "/home/rphelps/.cargo/bin"
+set -x JAVA_HOME /usr/lib/jvm/java-1.11.0-openjdk-amd64
+alias dotfiles="git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
