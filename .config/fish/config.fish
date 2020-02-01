ca64f42f96c6915cc22eedd354205835655dc2df
Ryan Phelps <rphelps@apexclearing.com>
2020-02-01 14:10:38 -0800

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,11 @@
-set PATH \
-    "/home/rphelps/Android/Sdk/platform-tools" \
+set -x PATH \
     $PATH \
-    "/home/rphelps/Dropbox/bin" \
-    "/home/rphelps/.local/bin" \
-    "/home/rphelps/flutter/bin" \
-    "/home/rphelps/.cargo/bin"
-set -x JAVA_HOME /usr/lib/jvm/java-1.11.0-openjdk-amd64
-alias dotfiles="git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+    "/home/rphelps/bin" \
+    "/home/rphelps/bin/google-cloud-sdk/bin" \
+    "/home/rphelps/bin/go/bin" \
+    "/home/rphelps/bin/kafka/kafka_2.12-2.2.0/bin"
+set -x fish_color_operator red
+set -x DOCKER_HOST tcp://localhost:2375
+
+# Less need for this w/ the java8 and java11 functions
+# alias groovysh='env JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64 /usr/bin/groovysh'
