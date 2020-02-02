45fdb3d8f2b56be191e4a2ca0fff736ce179e264
Ryan Phelps <rphelps@apexclearing.com>
2020-02-01 16:10:49 -0800

    dotfiles command alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,5 +7,6 @@ set -x PATH \
 set -x fish_color_operator red
 set -x DOCKER_HOST tcp://localhost:2375
 
+alias dotfiles="git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
 # Less need for this w/ the java8 and java11 functions
 # alias groovysh='env JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64 /usr/bin/groovysh'
