521a5ff764152f62de3c8876aa49cbab94a61ba2
Ryan Phelps <rphelps@apexclearing.com>
2020-02-02 20:47:46 -0800

    Use /home/rphelps and make

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,13 @@
-set -x PATH \
-    $PATH \
-    "/home/rphelps/bin" \
-    "/home/rphelps/bin/google-cloud-sdk/bin" \
-    "/home/rphelps/bin/go/bin" \
-    "/home/rphelps/bin/kafka/kafka_2.12-2.2.0/bin"
+set -x PATH                                \
+    $PATH                                  \
+    "$HOME/bin"                            \
+    "$HOME/bin/google-cloud-sdk/bin"       \
+    "$HOME/bin/go/bin"                     \
+    "$HOME/bin/kafka/kafka_2.12-2.2.0/bin"
 set -x fish_color_operator red
 set -x DOCKER_HOST tcp://localhost:2375
 
 alias dotfiles="git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+
 # Less need for this w/ the java8 and java11 functions
 # alias groovysh='env JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64 /usr/bin/groovysh'
