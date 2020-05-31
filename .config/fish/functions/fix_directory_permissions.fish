0dce2592fb99218b84145428492bf16928ff05e7
Ryan Phelps <rphelps@apexclearing.com>
2020-05-30 19:56:25 -0700

    Fix fix_directory_permissions function

--- .config/fish/functions/fix_directory_permissions.fish
+++ .config/fish/functions/fix_directory_permissions.fish
@@ -1,3 +1,3 @@
 function fix_directory_permissions
-    find . -perm -g+w -or -perm -o+w -print0 | xargs -0 chmod g-w,o-w 
+    find . \( -perm -g+w -or -perm -o+w \) -and -print0 | xargs -0 chmod g-w,o-w 
 end
